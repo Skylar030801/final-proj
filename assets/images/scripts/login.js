e488d8465ed714a50e87743f7cfc971cc47a94ba
skylarH <elantrahufkie@gmail.com>
2021-03-02 16:07:54 +0200

    update

--- assets/images/scripts/login.js
+++ assets/images/scripts/login.js
@@ -6,19 +6,19 @@ function admin() {
   let password = inputs[1].value;
 
   let users;
-  fetch("https://git.heroku.com/infinite-plains-20079.git")
+  fetch("https://pure-stream-15016.herokuapp.com/")
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
       user = json;
       console.log(password, username, user);
 
-      let logged = user.filter((user) => {
+      let logged = user.filter((users) => {
         return user.username == username && user.password == password;
       });
 
       if (logged.length >= 1) {
-        window.location.href = "./admin.html";
+        window.location.href = "./login-form.html";
       }
     });
 }
